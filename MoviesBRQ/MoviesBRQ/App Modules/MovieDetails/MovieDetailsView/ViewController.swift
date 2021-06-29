be41c9f4cd20fae98fe069965ec699eb46bc918b
Marcos Amorim Rossi de Carvalho <marcosamorimrossidecarvalho@NBRJ030353.local>
2021-06-29 17:09:25 -0300

    Commit Inicial

---
+++ MoviesBRQ/MoviesBRQ/App Modules/MovieDetails/MovieDetailsView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesBRQ
+//
+//  Created by Marcos Amorim Rossi de Carvalho on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
